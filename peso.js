58054156ae27e0afacabcc1ce6d6eba35537ed03
Carlos Medina <crmg.camg@gmail.com>
2022-12-11 17:10:02 -0300

    Comienzo Proyecto

---
+++ peso.js
@@ -0,0 +1,9 @@
+var usuario = document.getElementById("name")
+var peso = document.getElementById("peso")
+var g_jupiter = 24.8;
+var g_tierra= 9.8;
+var g_martes = 3.7;
+
+var peso_final =parseInt(peso * g_martes / g_tierra) 
+
+console.log("usuario")
